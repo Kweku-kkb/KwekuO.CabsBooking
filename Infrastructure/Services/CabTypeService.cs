736c15148b4f2801444738e155c6042288dbed4f
Kweku-kkb <61571883+Kweku-kkb@users.noreply.github.com>
2021-07-28 13:52:39 -0400

    Buildprogress

--- Infrastructure/Services/CabTypeService.cs
+++ Infrastructure/Services/CabTypeService.cs
@@ -12,10 +12,12 @@ namespace Infrastructure.Services
     public class CabTypeService : ICabTypesService
     {
         private readonly ICabTypesRepository _cabTypesRepository;
+        private readonly IPlacesRepository _placesRepository;
 
-        public CabTypeService(ICabTypesRepository cabTypesRepository)
+        public CabTypeService(ICabTypesRepository cabTypesRepository, IPlacesRepository placesRepository)
         {
             _cabTypesRepository = cabTypesRepository;
+            _placesRepository = placesRepository;
         }
 
         public async Task<List<CabTypeModel>> GetAllCabTypes()
@@ -34,6 +36,8 @@ namespace Infrastructure.Services
             return cabCards;
         }
 
+        
+     
         public async Task<CabDetailsResponseModel> GetCabDetails(int id)
         {
             var cabTypes = await _cabTypesRepository.GetByIdAsync(id);
@@ -45,8 +49,13 @@ namespace Infrastructure.Services
             };
 
             cabDetails.Bookings = new List<BookingsModel>();
+            cabDetails.Places = new List<PlacesModel>();
             foreach (var booking in cabTypes.Bookings)
             {
+                int To_Id = booking.ToPlace == null ? -1 : (int)booking.ToPlace;
+                var Placeto = await _placesRepository.GetByIdAsync((int) booking.ToPlace);
+                var FromPlace = await _placesRepository.GetByIdAsync((int)booking.FromPlace);
+
                 cabDetails.Bookings.Add(new BookingsModel
                 {
                     Id = booking.Id,
@@ -63,21 +72,12 @@ namespace Infrastructure.Services
                     ContactNo = booking.ContactNo,
                     Status = booking.Status
                 });
-
+                var PlacetoModel = new PlacesModel() { Id = Placeto.PlaceId, Name = Placeto.PlaceName };
+                var PlacefromModel = new PlacesModel() { Id = FromPlace.PlaceId, Name = FromPlace.PlaceName };
+                cabDetails.Places.Add(PlacetoModel);
+                cabDetails.Places.Add(PlacefromModel);
             }
-
-            //cabDetails.Places = new List<PlacesModel>();
-            //foreach (var place in cabTypes.Places)
-            //{
-            //    cabDetails.Places.Add(new PlacesModel
-            //    {
-            //        Id = place.Id,
-            //        Name = place.Name
-            //    });
-            //}
-
-            //return cabDetails;
-
+            return cabDetails;
         }
     }
 }
